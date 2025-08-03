8b204feb9defaf322c986f2a34300153bdf4a789
Diego Vazquez <Diegovazq37@gamil.com>
2025-08-03 09:58:03 -0300

    feat:formulario de registro con validaciones y ux minima

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,14 @@
+import { Layout } from "../components/Layout"
 
 const NotFound=()=>{
-
+return(
+    <Layout>
+        <h1>Pagina no encontrada</h1>
+        <p>Lo sentimos , la pagina que estas buscando no existe.</p>
+        <p>Verifica URL o volve al inicio.</p>
+        <link to="/">Ir al inicio</link>
+    </Layout>
+)
 }
 
 export {NotFound}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,22 +1,60 @@
+import { useState } from "react"
 import { Layout } from "../components/Layout"
 
 const Register= ()=>{
+    const [username,setUsername]= useState("")
+    const [email,setEmail]= useState("")
+    const [password,setPassword]= useState("")
+    const [error, setError]=useState("")
+    const [succes, setSucces]= useState("")
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        setError("")
+        setSucces("")
+       
+        if(!username || !email || !password){
+            setError("Debes cargar todos los campos")
+            return
+        }
+        const newUser={
+            username,
+            email,
+            password
+        }
+        console.log(newUser)
+        setSucces("Usuario creado con exito!!!")
+           setUsername("")
+        setEmail("")
+        setPassword("") 
+
+    }
 return(
     <Layout>       
-    <h1>Inicia sesion</h1>
+    <h1>Registrate</h1>
          <section>
-            <h2>Hola,bienvenido de nuevo</h2>
-            <form>
+            <h2>Hola,bienvenido</h2>
+            <form onSubmit={handleSubmit}>
                   <div>
                     <label>Username:</label>
-                    <input type="text"/>
-                </div>
+                    <input type="text" onChange={(e)=>setUsername(e.target.value)} value={username}/>
+                   </div>
                    <div>
+                    <label>Correo electronico:</label>
+                    <input type="email" onChange={(e)=>setEmail(e.target.value)}value={email}/>
+                    </div>
+                     <div>
                     <label>Contraseña:</label>
-                    <input type="password" />
+                    <input type="password"onChange={(e)=>setPassword(e.target.value)}value={password} />
+                    </div>
                     <button>Ingresar </button>
-                </div>
             </form>
+            {
+                error && <p style={{color:"red"}}>{error}</p>
+            }
+            {
+                succes && <p style={{color:"green"}}>{succes}</p>
+            }
          </section>
 
         </Layout>
