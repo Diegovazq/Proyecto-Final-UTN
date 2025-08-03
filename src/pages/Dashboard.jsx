329fecd79c89130f9b9a8f0b831b89b141746ef4
Diego Vazquez <Diegovazq37@gamil.com>
2025-08-02 22:26:42 -0300

    add: contenido estatico en dashbiard, preparacion de form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,30 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Panel de Administración</h1>
+      <h2>Cargar nuevo producto</h2>
+      <form>
+        <div>
+          <label >Nombre del producto:</label>
+          <input type="text" name="nombre" />
+        </div>
 
-const Dashboard=()=>{
+        <div>
+          <label >Precio:</label>
+          <input type="number"  name="precio" />
+        </div>
 
-}
+        <div>
+          <label >Descripción:</label>
+          <textarea name="descripcion"></textarea>
+        </div>
 
-export{Dashboard}
+        <div>
+          <button>Cargar producto</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export { Dashboard };
