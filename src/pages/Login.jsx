b30f9795971ef7227e7e2f4c3fe2e66e660229ba
Diego Vazquez <Diegovazq37@gamil.com>
2025-08-02 22:36:33 -0300

    add: contenido estatico en login, creacion del form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,27 @@
+import { Layout } from "../components/Layout"
+
+
 const Login=()=>{
+    return(
+        <Layout>
+            <h1>Inicia sesion</h1>
+         <section>
+            <h2>Hola,bienvenido de nuevo</h2>
+            <form>
+                  <div>
+                    <label>Correo electronico:</label>
+                    <input type="email"/>
+                </div>
+                   <div>
+                    <label>Contraseña:</label>
+                    <input type="password" />
+                    <button>Ingresar </button>
+                </div>
+            </form>
+         </section>
+
+        </Layout>
+    )
 
 }
 
