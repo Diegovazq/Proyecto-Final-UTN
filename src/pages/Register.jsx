c57611da269dd10b81fc912d94ddd71a15ba9c50
Diego Vazquez <Diegovazq37@gamil.com>
2025-08-02 22:45:25 -0300

    add: contenido estatico en register,preparacion de form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,26 @@
+import { Layout } from "../components/Layout"
 
 const Register= ()=>{
+return(
+    <Layout>       
+    <h1>Inicia sesion</h1>
+         <section>
+            <h2>Hola,bienvenido de nuevo</h2>
+            <form>
+                  <div>
+                    <label>Username:</label>
+                    <input type="text"/>
+                </div>
+                   <div>
+                    <label>Contraseña:</label>
+                    <input type="password" />
+                    <button>Ingresar </button>
+                </div>
+            </form>
+         </section>
 
+        </Layout>
+)
 }
 
 export {Register}
