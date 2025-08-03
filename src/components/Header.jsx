43fe5c09020b4ff1e66ebf30472bea226b7f9590
Diego Vazquez <Diegovazq37@gamil.com>
2025-08-02 21:53:39 -0300

    change: elementos a por link de react-router-dom en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom"
+
 const Header= ()=>{
     return(
         <header>
             <img src="" alt=""/>
             <nav>
                 <ul>
-                    <li><a href="/">Inicio</a></li>
-                      <li><a href="/dashboar">Dasboard</a></li>
-                        <li><a href="/Login">Login</a></li>
-                          <li><a href="/Registrate">Registrate</a></li>
+                    <li><Link to="/">Inicio</Link></li>
+                      <li><link to="/dashboar">Dasboard</link></li>
+                        <li><link to="/Login">Login</link></li>
+                          <li><link to="/Registrate">Registrate</link></li>
                           <button>Cerrar sesion</button>
                 </ul>
             </nav>
